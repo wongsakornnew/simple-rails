a6a883509d43e6b19b9bc438a38cbc03c9a4f807
new <wongsakorn-_-new@hotmail.com>
2010-09-29 16:01:22 +0700

    add polymorphic Comment

---
+++ app/models/comment.rb
@@ -0,0 +1,4 @@
+class Comment < ActiveRecord::Base
+  attr_accessible :author, :email, :body, :commentable_id, :commentable_type
+  belongs_to :post
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100929084554) do
+ActiveRecord::Schema.define(:version => 20100929085701) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "author"
+    t.text     "body"
+    t.integer  "commentable_id"
+    t.string   "comment_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
